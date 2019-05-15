98241d5b939a418bfa58e690a018730356c3b9d2
吴佳雄 <724151462@qq.com>
2019-05-15 18:39:07 +0800

    'json'

--- src/main/java/com/example/test/controller/Book.java
+++ src/main/java/com/example/test/controller/Book.java
@@ -1,7 +1,10 @@
 package com.example.test.controller;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class Book implements Serializable {
     private Integer id;
     private String name;
